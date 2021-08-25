a79ed73544f1fc6013cebd13cb9a43a7a5cf5daf
Akhil Kumar Adepu <akhil9394021176@gmail.com>
2021-08-25 20:20:38 +0530

    User added to database

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const User = require('../models/user.model');
+
+const router = express.Router();
+
+router.post("", async (req, res) => {
+
+    const user = await User.create(req.body);
+
+    return res.send(user)
+});
+
+router.get("", async (req, res) => {
+    const users = await User.find().lean().exec();
+
+    return res.send(users);
+});
+
+router.get("/:id", async (req, res) => {
+    const user = await User.findById(req.params.id).lean().exec();
+
+    return res.send(user);
+});
+
+router.delete("/:id", async (req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    return res.send(user);
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -4,11 +4,15 @@ const connect = require("./configs/db");
 
 const productController = require("./controllers/product.controller")
 
+const userController = require("./controllers/user.controller")
+
 const app = express();
 
 app.use(express.json());
 
 app.use("/products", productController);
+
+app.use("/users", userController);
 
 app.listen(2345, async () => {
     await connect();
