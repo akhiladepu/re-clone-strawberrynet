f2c0d31b9cb8aa7287ae71561bbcfdf35c529422
Akhil Kumar Adepu <akhil9394021176@gmail.com>
2021-09-16 21:30:50 +0530

    sample testing

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
     return mongoose.connect("mongodb+srv://akhil:akhil@cluster0.tg80g.mongodb.net/sample?retryWrites=true&w=majority", {
-                useNewUrlParser: true,// <password> => akhil
-                useCreateIndex: true,
-                useUnifiedTopology: true,
-                useFindAndModify: false
-            });
+        useNewUrlParser: true,// <password> => akhil
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    });
 }
